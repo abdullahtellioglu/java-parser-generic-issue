b7dd9b580f386d3258788f9c0835ddf710acf816
abdullahtellioglu <abdullah@vaadin.com>
2025-12-09 13:35:40 +0300

    JarTypeSolver is added

--- src/main/java/com/example/application/JavaParserTest.java
+++ src/main/java/com/example/application/JavaParserTest.java
@@ -7,8 +7,10 @@ import com.github.javaparser.ast.expr.LambdaExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
@@ -23,6 +25,17 @@ public class JavaParserTest {
         JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
         parserConfiguration.setSymbolResolver(symbolSolver);
         StaticJavaParser.setConfiguration(parserConfiguration);
+        String classPath = System.getProperty("java.class.path");
+        for (String entry : classPath.split(File.pathSeparator)) {
+            if(entry.endsWith(".jar")) {
+                try{
+                    combinedTypeSolver.add(new JarTypeSolver(entry));
+                }catch (IOException ex){
+                    System.out.println("Can't find class path: " + entry);
+                }
+
+            }
+        }
         Path path = Path.of("./src/main/java/com/example/application/examplefeature/ui/TaskListView.java");
         Path normalized = path.toAbsolutePath().normalize();
         CompilationUnit cu = StaticJavaParser.parse(normalized);
